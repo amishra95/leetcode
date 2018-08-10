a60407971ea168e5982ccee0fb5a3906460d87ab
Avanish Mishra <avanish.neham@gmail.com>
2018-08-10 15:10:08 -0400

    leetcode solution

--- src/searchInsertPosition.java
+++ src/searchInsertPosition.java
@@ -1,4 +1,15 @@
 
 public class searchInsertPosition {
 
+    public int searchInsert(int[] nums, int target) {
+        
+        int index=nums.length;
+        for(int i=0; i<nums.length; i++ ){            
+            if( target <= nums[i] ){
+                index= i;
+                break;
+            }
+        }
+        return index;        
+    }
 }
