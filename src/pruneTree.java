340beac5f62b1c42cfdfd2d765c7875c986fece4
Avanish Mishra <avanish.neham@gmail.com>
2018-08-10 15:30:12 -0400

    Leetcode Solution

---
+++ src/pruneTree.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class pruneTree {
+    public TreeNode pruneTree(TreeNode root) {
+   if(root == null) return null;
+        
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+        
+    if(root.val == 0 && root.left == null && root.right == null) return null;
+        
+        return root;
+    }
+}
