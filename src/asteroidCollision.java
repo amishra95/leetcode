75a62ffbb6f79a9747ac985267ce143a3dead156
Avanish Mishra <avanish.neham@gmail.com>
2018-08-10 15:26:57 -0400

    leetcode 735

---
+++ src/asteroidCollision.java
@@ -0,0 +1,17 @@
+
+public class asteroidCollision {
+	 public int[] asteroidCollision(int[] asteroids) {
+	        
+		    Stack<Integer> stack = new Stack();
+		        asteroids:
+		        for(int ast : asteroids){
+		            while(!stack.isEmpty() && ast < 0 && 0 < stack.peek())
+		                if(-ast < stack.peek() ||  -ast == stack.pop())
+		                    continue asteroids;
+		            stack.push(ast);
+		        }
+		        return stack.stream().mapToInt(i->i).toArray();
+		        
+		    
+		}
+}
