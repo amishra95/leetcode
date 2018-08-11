3178710066677b3e2310824bfff91058ab289e62
Avanish Mishra <avanish.neham@gmail.com>
2018-08-11 18:47:29 -0400

    Leetcode Solution

---
+++ src/increasingArray.java
@@ -0,0 +1,15 @@
+
+public class increasingArray {
+	 public boolean checkPossibility(int[] nums) {
+	        int cnt = 0, n = nums.length; 
+	        for (int i = 0; i < n - 1; i++) {
+	            if (nums[i] > nums[i + 1]) {
+	                cnt++;
+	                if (i >= 1 && nums[i + 1] < nums[i - 1])
+	                    nums[i + 1] = nums[i]; 
+	            } 
+	            if (cnt > 1) return false; 
+	        }
+	        return true;
+	    }
+}
