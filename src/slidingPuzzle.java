c0da17b7cf9094d11004da26200e6a822bd050a4
Avanish Mishra <avanish.neham@gmail.com>
2018-08-11 19:04:54 -0400

    UNFINISHED STILL WORKING

---
+++ src/slidingPuzzle.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int slidingPuzzle(int[][] board) {
+            
+    if(board.length == 0 || board[0].length == 0){
+        return -1;
+    }
+        
+        
+        Queue<Integer> queue = new LinkedList<>();
+        ArrayList<    
+        
+        
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
+                if(puzzleCheck(int[][] board) == true){
+                    continue;            
+                }
+                
+                
+            }
+        }
+        
+    }
+    
+    public boolean puzzleCheck(int[][] board){
+     if(board[i][j] == [[1, 2, 3][4, 5, 0]]){
+         return true;
+     }
+        else
+        {
+            return false;
+        }
+}
+    
+public int swap(int a, int b, int[][] board){
+    int t = board[a];
+    board[a] = board[b];
+    board[b] = t;
+        
+    }
+    
+    
+public int numSwaps(int a, int b, int[][] board){
+    
+    Queue<Integer> queue = new Queue<>();
+    queue.add(board);
+    
+}
+    
+}
