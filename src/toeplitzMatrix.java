93df4ed529206e6ac9352b8413884a9d677223b6
Avanish Mishra <avanish.neham@gmail.com>
2018-08-12 10:00:06 -0400

    Leetcode Easy Solution

---
+++ src/toeplitzMatrix.java
@@ -0,0 +1,16 @@
+
+public class toeplitzMatrix {
+	public boolean isToeplitzMatrix(int[][] matrix) {
+		
+		for(int i = 0; i < matrix.length; i++) {
+			for(int j = 0; j < matrix[0].length; j++) {
+				if(matrix[i][j] != matrix[i+1][j+1]) {
+					return false;
+				}
+			}
+		}
+		
+		return true;
+		
+	}
+}
