6c46cdc1c75a67a7d5fa0d63b5ac36604a174c44
Avanish Mishra <avanish.neham@gmail.com>
2018-08-13 14:55:53 -0400

    Leetcode judgeSquare

---
+++ src/containsSquare.java
@@ -0,0 +1,27 @@
+import java.util.Set;
+
+public class containsSquare {
+	
+	public boolean judgeSquare(int c) {
+		
+		if(c < 0) {
+			return false;
+			
+		}
+		
+		HashSet<Integer> set = new HashSet<>();
+
+		
+
+	for(int i = 0; i <= c; i++) {
+		set.add(i*i);
+
+		if(set.contains(c- i*i)) {
+			return true;
+		}
+		
+	}
+	return false;
+
+	}
+}
