e3705660149dbe452cd3b198b1fbec94b538324c
Avanish Mishra <avanish.neham@gmail.com>
2018-08-14 15:34:32 -0400

    Unfinished question (needs Editing)

---
+++ src/thirdLargestNumber.java
@@ -0,0 +1,40 @@
+
+class Solution {
+    public int thirdMax(int[] nums) {
+      if(nums==null || nums.length < 3){
+            return 0;
+        }
+        
+        int count = 0;
+        
+        distinctArr(nums);
+        Arrays.sort(nums);
+
+    for(int i = nums.length - 1; i > 0; i--){
+    count = Math.min(nums[nums.length -2], nums[nums.length-3]);
+    
+}
+        return count;
+
+     
+        
+    }
+    
+    public int distinctArr(int arr[]){
+        
+        HashSet<Integer> set = new HashSet<>();
+        int[] arr2 = new int[arr.length];
+        
+    for(int i = 0; i < arr.length; i++){
+        if(!set.contains(arr[i])){
+            arr2.add(arr[i]);
+        }
+    }
+    
+        return arr2;
+    }
+
+    
+    
+        
+    }
