1e8db262dd60dfcd5aad4f4bba06d6913fc60bc4
Avanish Mishra <avanish.neham@gmail.com>
2018-08-16 17:52:45 -0400

    New Changes to original problem certified
    I more or less with the same method but there were couple of addition cases done here.

--- src/reorderedPowerOf2.java
+++ src/reorderedPowerOf2.java
@@ -1,36 +1,40 @@
 class Solution {
-    private boolean isPowerOfTwo (String s)
-    {
-        int x = Integer.parseInt(s);
-        return x!=0 && ((x&(x-1)) == 0);
+    public boolean reorderedPowerOf2(int N) {
+        return permutation(s, 0);
     }
     
-    private void swapChar(StringBuilder str, int i, int j){
-        char temp = str.charAt(i);
-        str.setCharAt(i,str.charAt(j));
-        str.setCharAt(j,temp);
+    public void convertString(int N){
+        StringBuilder sb = new StringBuilder();
+        sb.append(N);
+        String str = sb.toString();
     }
     
-    public boolean findPermutation(StringBuilder str, int pos){
+   
+    public String swapChars(String str, int lIdx, int rIdx){
+        StringBuilder sb = new StringBuilder(str);
+        char l = sb.charAt(lIdx);
+        char r = sb.charAt(rIdx);
+        return sb.toString();
+        
+    }
+    
+    public boolean permutation(StringBuilder sb, int pos){
         if(pos==str.length()&& isPowerOfTwo(str.toString())){
             return true;
         }
-        for(int i=pos; i< str.length();++i){
-            swapChar(str,pos,i);
-            if(str.charAt(0)!='0'){
-                if(findPermutation(str,pos+1)){
+        
+        for(int i = pos; i < str.length(); i++){
+            swapChar(str, pos, i);
+            if(str.charAt(0) != '0'){
+                if(permutation(str, pos+1)){
                     return true;
                 }
             }
-            swapChar(str,pos,i);
+            swapChars(str, pos, i);
         }
         return false;
     }
     
-    public boolean reorderedPowerOf2(int N) {
-        StringBuilder s = new StringBuilder();
-        s.append(Integer.toString(N));
-        return findPermutation(s,0);
-    }
+    
+    
 }
-
