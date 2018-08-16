141589a34b5806c8c3e03c139c997348fbb8ab87
Avanish Mishra <avanish.neham@gmail.com>
2018-08-16 17:40:11 -0500

    Incomplete Case till 6:39 Pm on 08/16
    First brief uncompiled Solution

---
+++ src/matchsticksToSquare.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean makesquare(int[] nums) {
+        if(nums == null || nums.length == 0){
+            return false;
+        }
+        if(squareCheck(nums) == true){
+            return true;
+        }
+    return false;
+        
+        
+    }
+    
+   public int countOccurences(int[] nums, int n, int x)
+   {
+       int count = 0;
+       for(int i = 0; i < n; i++){
+           if(x == nums[i])
+               count++;
+           return count;
+    }
+       
+       
+    public boolean squareCheck(int[] nums){
+        countOccurences(nums, n, x);
+        
+    if(count == 4 ||  count == 9 || count == 16){
+        return true;        
+    }
+    else {
+          
+    }
+        
+        
+    }
+        
+        
+        
+    }
+
+   
+   
+    }
