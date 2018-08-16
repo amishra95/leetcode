31e5c73b2a4fd345dfed6ecead4b4cb3dc18e598
Avanish Mishra <avanish.neham@gmail.com>
2018-08-15 23:54:41 -0400

    Add files via upload

--- src/reorderedPowerOf2.java
+++ src/reorderedPowerOf2.java
@@ -1,15 +1,36 @@
-
-public class reorderedPowerOf2 {
-	
-	    public boolean reorderedPowerOf2(int N) {
-	        if(N = 0 || N.length == 0 || N < 0){
-	            return false;
-	        }
-	    
-	        if(N == 1 || N == 2 || N == 4 || N == 8){
-	            return true;
-	        }
-	        
-	  }
-
+class Solution {
+    private boolean isPowerOfTwo (String s)
+    {
+        int x = Integer.parseInt(s);
+        return x!=0 && ((x&(x-1)) == 0);
+    }
+    
+    private void swapChar(StringBuilder str, int i, int j){
+        char temp = str.charAt(i);
+        str.setCharAt(i,str.charAt(j));
+        str.setCharAt(j,temp);
+    }
+    
+    public boolean findPermutation(StringBuilder str, int pos){
+        if(pos==str.length()&& isPowerOfTwo(str.toString())){
+            return true;
+        }
+        for(int i=pos; i< str.length();++i){
+            swapChar(str,pos,i);
+            if(str.charAt(0)!='0'){
+                if(findPermutation(str,pos+1)){
+                    return true;
+                }
+            }
+            swapChar(str,pos,i);
+        }
+        return false;
+    }
+    
+    public boolean reorderedPowerOf2(int N) {
+        StringBuilder s = new StringBuilder();
+        s.append(Integer.toString(N));
+        return findPermutation(s,0);
+    }
 }
+
