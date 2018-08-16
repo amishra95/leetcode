3aa92edcc7a9b564df8a615346bd058bee7869a8
Avanish Mishra <avanish.neham@gmail.com>
2018-08-15 20:29:11 -0500

    Incorrect and Incomplete
    This part is incomplete and hence working towards it's completion

---
+++ src/reorderedPowerOf2.java
@@ -0,0 +1,15 @@
+
+public class reorderedPowerOf2 {
+	
+	    public boolean reorderedPowerOf2(int N) {
+	        if(N = 0 || N.length == 0 || N < 0){
+	            return false;
+	        }
+	    
+	        if(N == 1 || N == 2 || N == 4 || N == 8){
+	            return true;
+	        }
+	        
+	  }
+
+}
