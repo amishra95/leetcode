4f8a7273f66d6e275fcbd868e7b3ee0bb8e6928e
Avanish Mishra <avanish.neham@gmail.com>
2018-08-22 19:38:49 -0400

    New added solution to the index

--- src/triangle.java
+++ src/triangle.java
@@ -10,3 +10,27 @@ public class triangle {
 	    return A[0];
 	}
 }
+
+
+// The below solution was a 2.4% fast solution
+
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        return helper(triangle, 0, 0, new HashSet<>());
+    }
+    public int helper(List<List<Integer>> triangle, int x, int y, Set<String> visited){
+        if(x >= triangle.size() || y >= triangle.size()){
+            return 0;
+        }
+        if(visited.contains(x + " " + y)){
+            return triangle.get(x).get(y);
+        }
+        int left = helper(triangle, x+1, y, visited);
+        int right = helper(triangle, x+1, y+1, visited);
+        int sum = Math.min(left, right) + triangle.get(x).get(y);
+        triangle.get(x).set(y, sum);
+        visited.add(x + " " + y);
+        return sum;
+        
+    }
+}
