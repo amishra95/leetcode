2d73a0b5c612fda12f9912f4b857c64713d61e7f
Avanish Mishra <avanish.neham@gmail.com>
2018-08-22 20:02:00 -0400

    Update triangle.java

--- src/triangle.java
+++ src/triangle.java
@@ -34,3 +34,26 @@ class Solution {
         
     }
 }
+
+
+// THis is the fastest Solution
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+	if(triangle == null || triangle.size() == 0)
+            return 0;
+    int n = triangle.size();
+    int[] dp = new int[n+1];
+        
+for(int i = n-1;i >= 0; i--){
+    List<Integer> list = triangle.get(i);
+   for(int j = 0; j <= i; j++){
+       dp[j]= list.get(j) + Math.min(dp[j], dp[j+1]);
+   } 
+   
+    
+    
+}        
+    return dp[0];
+        
+    }
+}
