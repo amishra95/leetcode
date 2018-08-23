eb82723036a7a31e0ea5c01eca4a2876300a630b
Avanish Mishra <avanish.neham@gmail.com>
2018-08-23 00:01:28 -0400

    Add files via upload

---
+++ src/binaryTreeFromPreOrderAndPost.java
@@ -0,0 +1,32 @@
+
+public class binaryTreeFromPreOrderAndPost {
+
+	/**
+	 * Definition for a binary tree node.
+	 * public class TreeNode {
+	 *     int val;
+	 *     TreeNode left;
+	 *     TreeNode right;
+	 *     TreeNode(int x) { val = x; }
+	 * }
+	 */
+	class Solution {
+	    public TreeNode constructFromPrePost(int[] pre, int[] post) {
+	        
+	        if(pre == null)
+	            return 0;
+	        if(pre == 1){
+	            return node;
+	        }
+	        
+	    }
+	    
+	    public int[] helper(int[] pre, int[] post){
+
+	        int[] array = new int[pre.length+1];
+	        array.fill(pre[0]);
+	        
+	        
+	    }
+	}
+}
