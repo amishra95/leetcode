ca2babf6cf02df0270a6bd91745efe066a10fe09
Avanish Mishra <avanish.neham@gmail.com>
2018-08-25 18:47:03 -0500

    Add files via upload

--- src/numberOfMatchingSubSequences.java
+++ src/numberOfMatchingSubSequences.java
@@ -1,29 +1,53 @@
-
-public class numberOfMatchingSubSequences {
-
-	public int numMatchingSubseq(String S, String[] words) {
-        // Array and sort it and then find the different solutions which exist here
-    int count = 0;
-    for(int i = 0; i < words.length; i++){
-        HashSet<String> set = new HashSet<>();
-       set.put(S);   
-        if(set.contains(words)){
-            count++;
+class Solution {
+    public int numMatchingSubseq(String S, String[] words) {
+        int ans=0;
+        Set<String> valid=new HashSet<>();
+        Set<String> invalid=new HashSet<>();
+        for(String word:words)
+        {
+            
+            if(valid.contains(word)) 
+            {
+                ans++;
+                continue;
+            }
+            if(invalid.contains(word))
+            {
+                continue;
+            }
+            else if(isSubseq(S,word))
+            {
+                valid.add(word);
+                ans++;
+            }
+            else
+            {
+                invalid.add(word);
+            }
+            
+        }
+        return ans;
     }
-}
- return count;
-     }
-    
-    public boolean isSubsequence(String S, String[] words){
-    
-    if(words.length == 0 || words == null){
+    public boolean isSubseq(String S,String word)
+    {
+        int i=0;
+        int j=0;
+        while(i<S.length()&&j<word.length())
+        {
+            if(S.charAt(i)==word.charAt(j))
+            {
+                i++;
+                j++;
+            }
+            else
+            {
+                i++;
+            }
+            
+        }
+        if(j==word.length())return true;
         return false;
     }
-        
-    for(int i = 0; i < words.length; i++){
-        
-    }
+
     
-        
-    }
 }
