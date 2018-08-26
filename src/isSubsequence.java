a7d751a3dcdbd0ed9e8581b927b1577af515985c
Avanish Mishra <avanish.neham@gmail.com>
2018-08-25 20:19:22 -0400

    Add files via upload

--- src/isSubsequence.java
+++ src/isSubsequence.java
@@ -1,60 +1,26 @@
 
 public class isSubsequence {
-
+	public boolean isSubsequence(String s, String t) {
 	
-	    public boolean isSubsequence(String s, String t) {
-	        if(s == null || s.length() == 0 || t == null || t.length() == 0)
-	            return true;
-	        
-	         Set<String> valid = new HashSet<>();
-	        Set<String> invalid = new HashSet<>();
-	        
-	        for(int i =0; i < t.length()-1; i++){
-	        for(int j  = i; j < s.length()-1; j++){
-	            if(valid.contains(s)){
-	                return true;
-	            }
-	            else if(invalid.contains(t)){
-	                return false;
-	            }
-	            else if(isSubseq(s, t)){
-	                valid.add(s);
-	                return true;
-	            }
-	            else{
-	                invalid.add(s);
-
-	            }
-	            
-	        }
-	        }
-	        return false;
-	           
-	        
+		if(s.length() == 0)
+			return true;
+	    
+		char[] c1 = s.toCharArray();
+		char[] c2 = t.toCharArray();
+		int i = 0;
+		int j = 0;
+		
+		while(i< c1.length && j < c2.length) {
+			if(c1[i] != c2[i]) {
+				j++;
+			}
+			else {
+				i++;
+				j++;
+			}
+		}
+		
+		return i == c1.length;
 	}
-	    
-	    
-	     public boolean isSubseq(String S,String word)
-	    {
-	        int i=0;
-	        int j=0;
-	        while(i<S.length()&&j<word.length())
-	        {
-	            if(S.charAt(i)==word.charAt(j))
-	            {
-	                i++;
-	                j++;
-	            }
-	            else
-	            {
-	                i++;
-	            }
-	            
-	        }
-	        if(j==word.length())return true;
-	        return false;
-	    }
-
-	    
-	}
+}
 
