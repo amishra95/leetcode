f7e7d4f9e612872aa70aa96c446c82a503b3e65a
Avanish Mishra <avanish.neham@gmail.com>
2018-08-27 13:25:23 -0400

    Update asteroidCollision.java

--- src/asteroidCollision.java
+++ src/asteroidCollision.java
@@ -4,7 +4,6 @@ public class asteroidCollision {
 	        
 		    Stack<Integer> stack = new Stack();
 		        asteroids:
-		 
 		        for(int ast : asteroids){
 		            while(!stack.isEmpty() && ast < 0 && 0 < stack.peek())
 		                if(-ast < stack.peek() ||  -ast == stack.pop())
