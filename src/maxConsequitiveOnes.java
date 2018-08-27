162dd48d5d2ea1de31a2f724dfa74384d6d6b474
Avanish Mishra <avanish.neham@gmail.com>
2018-08-27 10:36:39 -0400

    Update maxConsequitiveOnes.java

--- src/maxConsequitiveOnes.java
+++ src/maxConsequitiveOnes.java
@@ -1,4 +1,19 @@
 
 public class maxConsequitiveOnes {
-
+	    public int findMaxConsecutiveOnes(int[] nums) {
+	       
+	int count = 0;
+	    int nonCount = 0;
+	        
+	    for(int i = 0; i < nums.length-1; i++){
+	      if(nums[i] == 1){
+        count = Math.max(count, nonCount);
+        }
+     nonCount = 0;
+      }
+ return count;       
+      }
 }
+
+
+
