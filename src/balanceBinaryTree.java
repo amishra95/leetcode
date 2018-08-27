2dfd1ca5d9e337ae55e1a6c478564edc0565078a
Avanish Mishra <avanish.neham@gmail.com>
2018-08-27 13:04:21 -0400

    Update balanceBinaryTree.java

--- src/balanceBinaryTree.java
+++ src/balanceBinaryTree.java
@@ -1,20 +1,27 @@
 
-public class balanceBinaryTree {
-	public boolean isBalanced(TreeNode root) {
-	        return dfs(root) != -1;
-	    }
-	    public int dfs(TreeNode root){
-	        if(root == null)
-	            return 0;
-	        
-	        int leftDepth = dfs(root.left);
-	        int rightDepth = dfs(root.right);
-	        
-	        if(leftDepth == -1 || rightDepth ==  -1 || Math.abs(leftDepth- rightDepth) > 1)
-	        { return -1;
-	            }
-	    
-	    
-	    return Math.max(leftDepth, rightDepth) + 1;
-	    }
+class Solution {
+    
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        int leftHeight = getHeight(root.left);
+        int rightHeight = getHeight(root.right);
+        if (leftHeight == -1 || rightHeight == -1) {
+            return false;
+        }
+        return Math.abs(leftHeight - rightHeight) <= 1;
+    }
+    
+    private int getHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = getHeight(root.left);
+        int right = getHeight(root.right);
+        if (left == -1 || right == -1 || Math.abs(left - right) > 1) {
+            return -1;
+        }
+        return Math.max(left, right) + 1;
+    }
 }
