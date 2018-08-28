7d2546a13c277b36699cec7851c5e201975e3f30
Avanish Mishra <avanish.neham@gmail.com>
2018-08-27 23:57:24 -0400

    Update diameterOfBinaryTree.java

--- src/diameterOfBinaryTree.java
+++ src/diameterOfBinaryTree.java
@@ -9,15 +9,12 @@
  */
 public class Solution {
     int max = 0;
-    
-    public int diameterOfBinaryTree(TreeNode root) {
+public int diameterOfBinaryTree(TreeNode root) {
         maxDepth(root);
         return max;
     }
-    
-    private int maxDepth(TreeNode root) {
+ private int maxDepth(TreeNode root) {
         if (root == null) return 0;
-        
         int left = maxDepth(root.left);
         int right = maxDepth(root.right);
         
