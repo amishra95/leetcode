ae5e6c9c105ce346c8353be6ec83c3116146f646
Avanish Mishra <avanish.neham@gmail.com>
2018-08-27 23:56:39 -0400

    Update BestTimeToBuy1.java

--- src/BestTimeToBuy1.java
+++ src/BestTimeToBuy1.java
@@ -1,18 +1,12 @@
 
 public class BestTimeToBuy1 {
-
 	public int maxProfit(int[] prices) {
-		
 		int maxCur = 0;
 		int maxFar = 0;
-		
 	for(int i = 1; i < prices.length; i++) {
 		maxCur = Math.max(0,  maxCur += prices[i]- prices[i-1]);
 		maxFar = Math.max(maxCur, maxFar);
-		
-		
 	}
-	
-	return maxFar;
+return maxFar;
 	}
 }
