92fb51c7e28267d66ef4b8cd1da1b92bc842139e
Avanish Mishra <avanish.neham@gmail.com>
2018-08-28 14:55:08 -0400

    Incorrect Add
    This problem involves the use of DP; It's almost similar to the House Robber Problem

---
+++ src/predictTheWinner.java
@@ -0,0 +1,15 @@
+class Solution {
+    
+    public boolean PredictTheWinner(int[] nums) {
+    
+    int[] dp1 = new int[nums.length];
+    int[] used = new int[nums.length];
+
+   
+}
+    public int helper(int a, int b, int[] nums){
+        
+    
+    }
+    
+}
