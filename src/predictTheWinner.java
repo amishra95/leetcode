21c52025a877513d79ede1928ea78a8630ef73ba
Avanish Mishra <avanish.neham@gmail.com>
2018-08-28 20:32:11 -0400

    PredictThe Winner

--- src/predictTheWinner.java
+++ src/predictTheWinner.java
@@ -12,3 +12,19 @@ class Solution {
     return dp[nums.length -1] >= 0;
     }
 }
+
+// New Solution with 2D Array
+
+class Solution{
+	public boolean PredictTheWinner(int[] nums) {
+		int[][] dp = new int[nums.length+1][nums.length];
+		for(int s = nums.length; s>=0; s--) {
+			for(int e = s+1; e< nums.length;e++) {
+				int a = nums[s] - dp[s+1][e];
+				int b  = nums[e] - dp[s][e-1];
+				dp[s][e] = Math.max(a,b);
+			}
+		}
+		return dp[0][nums.length - 1]>=0;
+	}
+}
