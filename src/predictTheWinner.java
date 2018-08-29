6fd11e2a6f900110b40bc72f617a9c2a1e4ded87
Avanish Mishra <avanish.neham@gmail.com>
2018-08-28 20:11:00 -0400

    Update predictTheWinner.java

--- src/predictTheWinner.java
+++ src/predictTheWinner.java
@@ -1,15 +1,14 @@
 class Solution {
     
     public boolean PredictTheWinner(int[] nums) {
-    
-    int[] dp1 = new int[nums.length];
-    int[] used = new int[nums.length];
-
-   
-}
-    public int helper(int a, int b, int[] nums){
-        
-    
+    int[] dp = new int[nums.length];
+    for(int s = nums.length; s>= 0; s--) {
+    	for(int e = s+1; e < nums.length; e++) {
+    		int a = nums[s] -dp[e];
+    		int b = nums[e] - dp[e-1];
+    		dp[e] = Math.max(a, b);
+    	}
     }
-    
+    return dp[nums.length -1] >= 0;
+    }
 }
