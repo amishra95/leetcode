c5bd85ed18cd43161c0fe34a300c6eff5e747b7d
Avanish Mishra <avanish.neham@gmail.com>
2018-08-29 02:16:57 -0400

    New and fastest Solution

--- src/maximumFrequencyStack.java
+++ src/maximumFrequencyStack.java
@@ -1,43 +1,26 @@
 class FreqStack {
-    int pushCount;
-    PriorityQueue<Node> pq;
-    Map<Integer, Integer> map;
+    HashMap<Integer, Integer> freq;
+    HashMap<Integer, Stack<Integer>> m;
+    int maxfreq;
     public FreqStack() {
-        pushCount = 0;
-        pq = new PriorityQueue<Node>(new NodeComparator());
-        map = new HashMap<Integer, Integer>();
+        freq = new HashMap<>();
+        m = new HashMap<>();
+        maxfreq = 0;
     }
-    
-    public void push(int x) {
-        if (!map.containsKey(x)){
-            map.put(x, 0);
-        }
-        map.put(x, map.get(x) + 1);
-        Node n = new Node(x, map.get(x), pushCount++);
-        pq.offer(n);
-    }
-    
-    public int pop() {
-        return pq.poll().value;
-    }
-    
-    
-    class Node{
-        public int value;
-        int freq;
-        int pushIndex;
-        public Node(int v, int f, int p){
-            this.value = v;
-            this.freq = f;
-            this.pushIndex = p;
-        }
-    }
-    
-    class NodeComparator implements Comparator<Node>{
-       public int compare(Node a, Node b){
-            if (a.freq != b.freq) return b.freq -a.freq;
-                    return b.pushIndex - a.pushIndex;
 
-        }
+    public void push(int x) {
+        int f = freq.getOrDefault(x, 0) + 1;
+        freq.put(x, f);
+        maxfreq = Math.max(maxfreq, f);
+        if (!m.containsKey(f)) m.put(f, new Stack<Integer>());
+        m.get(f).add(x);
     }
+
+    public int pop() {
+        int x = m.get(maxfreq).pop();
+        freq.put(x, maxfreq - 1);
+        if (m.get(maxfreq).size() == 0) maxfreq--;
+        return x;
+    }
+
 }
