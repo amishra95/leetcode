3a42e0622b18d748c7ae2cd988617fc06b03121a
Avanish Mishra <avanish.neham@gmail.com>
2018-08-30 13:32:06 -0500

    multiList addition

---
+++ src/flattenMulti.java
@@ -0,0 +1,26 @@
+class Solution {
+    public Node flatten(Node head) {
+        if(head == null) return head;
+        
+        Node p = head;
+    while(p != null){
+        if(p.child == null){
+            p = p.next;
+            continue;
+        }
+        
+    Node temp = p.child;
+        
+        while(temp.next != null)
+            temp = temp.next;
+        temp.next = p.next;
+        if(p.next != null) p.next.prev = temp;
+        p.next = p.child;
+        p.child.prev = p;
+        p.child = null;
+    }
+        return head;
+            
+        
+    }
+}
