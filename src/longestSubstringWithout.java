2cfed7e35d41a680d7cfbb87cac7807e7ea890ff
Avanish Mishra <avanish.neham@gmail.com>
2018-09-01 18:33:04 -0400

    Add files via upload

---
+++ src/longestSubstringWithout.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+       int max = 0; 
+        HashMap<Character, Integer> map = new HashMap<>();
+        int i = 0;
+        int count = 0;
+        for(int j = 0; j < s.length(); j++){
+            if(!map.containsKey(s.charAt(j))){
+                map.put(s.charAt(j), j);
+                count++;
+                if(max < count){
+                    max = count;
+                }
+            }
+            else{
+                if( i < s.length()){
+                    j = i;
+                }
+                i++;
+                count = 0;
+                map.clear();
+            }
+        
+    }  
+        return max;
+    
+    }
+}
