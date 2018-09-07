bf2a8d522ab0001e415d410445fc7fb3632911d2
Avanish Mishra <avanish.neham@gmail.com>
2018-09-07 10:18:35 -0400

    Sort Colors method

---
+++ src/sortColors.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int[] color = new int[3];
+        for(int i = 0; i < nums.length; i++){
+            color[nums[i]]++;
+        }
+        int curr = 0;
+        for(int i = 0; i < 3; i++){
+            
+            for(int j = 0; j < color[i]; j++){
+                nums[curr++] = i;
+            }
+        }
+    }
+}
