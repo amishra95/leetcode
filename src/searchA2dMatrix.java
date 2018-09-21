ebebd15a80cb6ad879a925dd3f4a34efdc1b5a3e
Avanish Mishra <avanish.neham@gmail.com>
2018-09-21 19:34:15 -0400

    2d Matrix search question

---
+++ src/searchA2dMatrix.java
@@ -0,0 +1,54 @@
+/**Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+Integers in each row are sorted from left to right.
+The first integer of each row is greater than the last integer of the previous row.
+Example 1:
+
+Input:
+matrix = [
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+target = 3
+Output: true
+Example 2:
+
+Input:
+matrix = [
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+target = 13
+Output: false
+*/
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+    
+        if(matrix.length == 0)
+            return false;
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        
+        int row = 0;
+        int col = cols -1;
+        
+       while(row <= rows-1 && col >= 0){ 
+        if(matrix[row][col] == target){
+            return true;
+        }
+       else if(matrix[row][col] < target){
+            row++;
+        }
+        
+        else if (matrix[row][col] > target){
+            col--;
+        }
+        }
+        return false;
+
+    }
+}
+
+}
