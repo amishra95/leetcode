1d9da1ca7bf4a740df17983bf4fe41dedc301f8f
Avanish Mishra <avanish.neham@gmail.com>
2018-09-23 12:52:16 -0400

    transpose Matrix

---
+++ src/transposeMatrix.java
@@ -0,0 +1,18 @@
+/**
+Given a matrix A, return the transpose of A.
+
+The transpose of a matrix is the matrix flipped over it's main diagonal, switching the row and column indices of the matrix.
+
+ */
+
+class Solution {
+    public int[][] transpose(int[][] A) {
+        int R = A.length, C = A[0].length;
+        int[][] ans = new int[C][R];
+        for (int r = 0; r < R; ++r)
+            for (int c = 0; c < C; ++c) {
+                ans[c][r] = A[r][c];
+            }
+        return ans;
+    }
+}
