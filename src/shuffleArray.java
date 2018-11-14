6f82631d40b87db7cda3b5d6f1d53beaf3970ebb
Avanish Mishra <avanish.neham@gmail.com>
2018-11-14 08:54:47 -0500

    shuffleArray (Design Question)

---
+++ src/shuffleArray.java
@@ -0,0 +1,32 @@
+public class Solution {
+
+    private int[] nums;
+    
+    public Solution(int[] nums) {
+        this.nums = nums;
+    }
+    
+    /** Resets the array to its original configuration and return it. */
+    public int[] reset() {
+        return nums;
+    }
+    
+    /** Returns a random shuffling of the array. */
+    public int[] shuffle() {
+        int[] rand = new int[nums.length];
+        for (int i = 0; i < nums.length; i++){
+            int r = (int) (Math.random() * (i+1));
+            rand[i] = rand[r];
+            rand[r] = nums[i];
+        }
+        return rand;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int[] param_1 = obj.reset();
+ * int[] param_2 = obj.shuffle();
+ */
+
