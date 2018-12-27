82bbe351e942587f70d373cace778adb87af3e28
Avanish Mishra <avanish.neham@gmail.com>
2018-12-27 23:13:33 +0530

    Update validMountainArray.java

--- src/validMountainArray.java
+++ src/validMountainArray.java
@@ -1,3 +1,12 @@
+Given an array A of integers, return true if and only if it is a valid mountain array.
+
+Recall that A is a mountain array if and only if:
+
+A.length >= 3
+There exists some i with 0 < i < A.length - 1 such that:
+A[0] < A[1] < ... A[i-1] < A[i]
+A[i] > A[i+1] > ... > A[B.length - 1]
+ 
 class Solution {
     public boolean validMountainArray(int[] A) {
         if(A == null || A.length < 3){
