8c7dcd49b8594c2044027ed7904bf168b25149b2
Avanish Mishra <avanish.neham@gmail.com>
2018-12-27 23:01:49 +0530

    valid Triangle

---
+++ src/validTriangleNumber.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class validTriangleNumber {
+	public int triangleNumber(int[] nums) {
+		
+		int count = 0;
+	if(nums.length == 0 || nums == null) {
+		return 0;
+	}
+	
+	if(nums.length < 3) {
+		return count;
+	}
+	Arrays.sort(nums);
+	for(int i = 2; i < nums.length; i++) {
+		int left = 0; int right = i-1;
+	while(left < right) {
+		if(nums[left] + nums[right] > nums[i]) {
+			count += (right- left);
+			right--;
+		}
+		else {
+			left++;
+		}
+	}
+	}
+return count;
+	}
+
+}
