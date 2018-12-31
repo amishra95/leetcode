77af5ec3007460d7e3ca1c62a843a9a644d2d212
Avanish Mishra <avanish.neham@gmail.com>
2018-12-31 13:49:11 +0530

    Update longestIncreasingSubseq.java

--- src/longestIncreasingSubseq.java
+++ src/longestIncreasingSubseq.java
@@ -1,25 +1,26 @@
-class Solution{
-public int lengthOfLIS(int[] nums) {
-if(nums==null || nums.length==0)
-return 0;
-ArrayList<Integer> list = new ArrayList<Integer>();
-for(int num: nums){
-if(list.size()==0 || num>list.get(list.size()-1)){
-list.add(num);
-}else{
-int i=0;
-int j=list.size()-1;
-while(i<j){
-int mid = (i+j)/2;
-if(list.get(mid) < num){
-i=mid+1;
-}else{
-j=mid;
-}
-}
-list.set(j, num);
-}
-}
-return list.size();
-}
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+        
+       int[] dp = new int[nums.length];
+    
+    for(int i = 0; i < nums.length; i++){
+        dp[i] = 1;
+        for(int j = 0; j < i; j++){
+            if(nums[i] > nums[j]){
+            dp[i] = Math.max(dp[i], dp[j] + 1);
+            }            
+        
+
+        }
+    }
+    int res = -1;
+        for(int i : dp){
+res = Math.max(i, res);
+        }
+        
+        return res;
+    }
 }
