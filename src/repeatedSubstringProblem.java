bf0af296a4301b4c4a1e62e23fab5e5fc970a1ed
Avanish Mishra <avanish.neham@gmail.com>
2019-01-08 22:52:32 +0530

    Update repeatedSubstringProblem.java

--- src/repeatedSubstringProblem.java
+++ src/repeatedSubstringProblem.java
@@ -1,15 +1,7 @@
-// Incorrect solution(Attempt 1)
+//correct changes using regex
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
-		char[] charArray = s.toCharArray();
-        Arrays.sort(charArray);
-    
-    for(int i = 0; i < charArray.length-1; i++){
-        if(charArray[i] != charArray[i+1]){
-            return true;
-        }
-    }
-        return false;
+		return s.matches("(.+)\\1+");
     }
 }
 
