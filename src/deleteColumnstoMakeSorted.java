3ea5f8bc4ad758bcabb3809189198bf7419d631e
Avanish Mishra <avanish.neham@gmail.com>
2019-01-09 22:38:29 +0530

    deleting columns

---
+++ src/deleteColumnstoMakeSorted.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minDeletionSize(String[] A) {
+   int count = 0;
+    
+    for(int i = 0; i < A[0].length(); i++){
+        for(int j = 1; j < A.length; j++){
+            if(A[j-1].charAt(i) > A[j].charAt(i)){
+                count++;
+                break;
+            }
+        }
+    }
+        return count;
+    }
+}
