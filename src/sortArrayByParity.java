f9b16c24baca1566afbfd8e434f4cb997bee9fbb
Avanish Mishra <avanish.neham@gmail.com>
2019-01-10 13:23:10 +0530

    sorting Array on basis of index and parity

--- src/sortArrayByParity.java
+++ src/sortArrayByParity.java
@@ -1,16 +1,37 @@
-class Solution {
-    public int[] sortArrayByParity(int[] A) {
-        int[] ans = new int[A.length];
-        int t = 0;
+/**Given an array A of non-negative integers, half of the integers in A are odd, and half of the integers are even.
 
-        for (int i = 0; i < A.length; ++i)
-            if (A[i] % 2 == 0)
-                ans[t++] = A[i];
+Sort the array so that whenever A[i] is odd, i is odd; and whenever A[i] is even, i is even.
 
-        for (int i = 0; i < A.length; ++i)
-            if (A[i] % 2 == 1)
-                ans[t++] = A[i];
+You may return any answer array that satisfies this condition.
 
-        return ans;
+ 
+
+Example 1:
+
+Input: [4,2,5,7]
+Output: [4,5,2,7]
+Explanation: [4,7,2,5], [2,5,4,7], [2,7,4,5] would also have been accepted.
+*/
+public class sortArrayByParity {
+	public int[] sortArrayByParityII(int[] A) {
+        int i = 0, j = 1, n = A.length;
+        while (i < n && j < n) {
+            while (i < n && A[i] % 2 == 0) {
+                i += 2;
+            }
+            while (j < n && A[j] % 2 == 1) {
+                j += 2;
+            }
+            if (i < n && j < n) {
+                swap(A, i, j);
+            }
+        }
+        return A;
     }
+    private void swap(int[] A, int i, int j) {
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
+
 }
