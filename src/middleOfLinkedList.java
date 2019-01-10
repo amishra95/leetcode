528c829e4193fc06c51a25a5b285a31126635d35
Avanish Mishra <avanish.neham@gmail.com>
2019-01-10 14:13:48 +0530

    splitting a LinkedList

---
+++ src/middleOfLinkedList.java
@@ -0,0 +1,26 @@
+Given a non-empty, singly linked list with head node head, return a middle node of linked list.
+
+If there are two middle nodes, return the second middle node.
+
+		/**
+		 * Definition for singly-linked list.
+		 * public class ListNode {
+		 *     int val;
+		 *     ListNode next;
+		 *     ListNode(int x) { val = x; }
+		 * }
+		 */
+		class Solution {
+		    public ListNode middleNode(ListNode head) {
+		        
+		    ListNode slow = head, fast = head;
+		        while(fast != null && fast.next != null){
+		            slow = slow.next;
+		            fast = fast.next.next;
+		        }
+		        
+		    return slow;
+		        
+
+		    }
+		}
