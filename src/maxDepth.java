8ad74ff4c74d83e613c7b94a7bdb15905529a74e
Avanish Mishra <avanish.neham@gmail.com>
2019-09-24 16:36:45 +0530

    Update maxDepth.java

--- src/maxDepth.java
+++ src/maxDepth.java
@@ -3,13 +3,12 @@ public class maxDepth {
 
 	public int maxDepth(TreeNode root) {
 		int max = 0;
-		
 	if(root == null)
 		return 0;
 	if(root.left == null && root.right == null) {
 		return 1;
 	}
-	
+	// recursive tool to loop through an entire left node and right node of a Binary Tree in java
 	max = Math.max(maxDepth(root.left), maxDepth(root.right))+1;
     return max;
     
