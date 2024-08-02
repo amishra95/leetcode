39698e89662a27490408f03e761e168d580f884a
Avanish Mishra <avanish.neham@gmail.com>
2024-08-02 22:56:53 +0530

    Update moveZeros.java

--- src/moveZeros.java
+++ src/moveZeros.java
@@ -13,12 +13,38 @@ class Solution {
             }
         }
         
-        }
-    
-    
     }
     
     
-        
+    }
+
+}
+
+
+// Solved with O(N)^2 time complexity which is as follows; @amishra95
+
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+
+      for(int i = 0; i < nums.length-1; i++){
+
+             swap(nums);
+      }
+
+}
+
+    //Create a swap method that swaps, integers
+    public void swap(int[] nums){
     
+        for(int i = 0; i < nums.length-1; i++){
+            if(nums[i]== 0 && nums[i+1] != 0){
+                int tmp = nums[i];
+                nums[i] = nums[i+1];
+                nums[i+1] = tmp;
+            }
+        }
+    }
+
+   
 }
