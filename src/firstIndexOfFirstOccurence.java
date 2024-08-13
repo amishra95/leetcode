f6a2bb8fdc13891123def3ba275e3b6bbba7cfcc
Avanish Mishra <avanish.neham@gmail.com>
2024-08-13 17:29:50 +0530

    Create firstIndexOfFirstOccurence.java

---
+++ src/firstIndexOfFirstOccurence.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if(needle.length() > haystack.length()){
+            return -1;
+        }
+
+        return haystack.indexOf(needle);
+
+
+
+
+    }
+}
