ec90e8896c2b395bd6db1dc9e3f359776f6a680b
Avanish Mishra <avanish.neham@gmail.com>
2024-08-15 18:34:51 +0530

    Update maxDepth.java

--- src/maxDepth.java
+++ src/maxDepth.java
@@ -14,3 +14,38 @@ public class maxDepth {
     
 	}
 }
+
+// Solution 2;
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+
+
+int max = 0;
+            if(root == null){
+                return 0;
+            }
+
+        int leftSum = maxDepth(root.left);
+        int rightSum = maxDepth(root.right);
+
+        max = Math.max(leftSum, rightSum);
+
+        return max+1;
+
+    }
+}
