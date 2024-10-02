0995de6ccf4c1c2f63f1435d1581a5d073cf861a
Avanish Mishra <avanish.neham@gmail.com>
2024-10-02 18:44:22 +0530

    Update lowestCommonAncestor.java

--- src/lowestCommonAncestor.java
+++ src/lowestCommonAncestor.java
@@ -1,19 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
 
-public class lowestCommonAncestor {
-
-	
-	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-		
-		
-		if(root==null||root==p||root==q) {
-            return root;
-        } 
-        TreeNode left, right;
-        left = lowestCommonAncestor(root.left,p,q);
-        right = lowestCommonAncestor(root.right, p, q);
-        if((left==p && right==q ) || (left==q&&right==p)) {
+        if(root == null){
             return root;
         }
-        return left==null?right:left;
-	}
+
+        if(root.val == p.val || root.val == q.val){
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if(left == null){
+            return right;
+        }
+
+        if(right == null){
+            return left;
+        }
+
+        return root;
+
+    }
 }
