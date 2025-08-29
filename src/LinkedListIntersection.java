7bc14ca43f84ed1079929f8cdf66ecdfcf4f08d4
Avanish Mishra <avanish.neham@gmail.com>
2025-08-30 05:02:30 +0530

    Update LinkedListIntersection.java

--- src/LinkedListIntersection.java
+++ src/LinkedListIntersection.java
@@ -8,7 +8,8 @@ public class LinkedListIntersection {
    ListNode b = headB;
    
    //if a & b have different len, then we will stop the loop after second iteration
-   while( a != b){
+   while( a != b)
+   { 
    	//for the end of first iteration, we just reset the pointer to the head of another linkedlist
        a = a == null? headB : a.next;
        b = b == null? headA : b.next;    
@@ -19,3 +20,4 @@ public class LinkedListIntersection {
 
 
 }
+
