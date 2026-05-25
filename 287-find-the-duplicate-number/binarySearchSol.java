298bcc8c7da213840576ad6ef3e1fef4050a5b1f
Avanish Mishra <avanish.neham@gmail.com>
2026-05-25 11:06:00 +0530

    Create binarySearchSol.java

---
+++ 287-find-the-duplicate-number/binarySearchSol.java
@@ -0,0 +1,22 @@
+    public static int findDuplicate_bs(int[] nums) {
+        int len = nums.length;
+        int low = 1;
+        int high = len - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            int cnt = 0;
+            for (int i = 0; i < len; i++) {
+                if (nums[i] <= mid) {
+                    cnt++;
+                }
+            }
+
+            if (cnt <= mid) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+
+        return low;
+    }
