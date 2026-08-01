2248bd93a99adecef7b277c8a28f5154ac30a941
Avanish Mishra <avanish.neham@gmail.com>
2026-08-02 01:05:13 +0530

    Time: 2 ms (95.92%) | Memory: 48.8 MB (32.52%) - LeetSync

---
+++ 338-counting-bits/counting-bits.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] countBits(int n) {
+
+  int[] array = new int[n+1];
+  
+    if(n == 0){
+        return array;
+    }
+
+    array[0] = 0;
+    array[1] = 1;
+
+    for(int i = 2; i <=n; i++){
+        array[i] = i %2 + array[i/2];
+
+    }
+
+    return array;
+  
+  }
+
+}
