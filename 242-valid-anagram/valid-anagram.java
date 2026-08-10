5ab87207d0117116c3286eba321209e95443231b
Avanish Mishra <avanish.neham@gmail.com>
2026-08-10 10:32:32 +0530

    Time: 13 ms (27.71%) | Memory: 46.3 MB (48.01%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,12 +1,22 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
 
-        char[] a = s.toCharArray();
-        char[] b = t.toCharArray();
-        Arrays.sort(a);
-        Arrays.sort(b);
+        if(t.length() != s.length()){
+            return false;
+        }
+    
+    HashMap<Character, Integer> map1 = new HashMap<>();
+    HashMap<Character, Integer> map2 = new HashMap<>();
 
-        return Arrays.equals(a, b);
-        
+    for(char b: s.toCharArray()){
+        map1.put(b, map1.getOrDefault(b,0)+1);
+    }
+
+    for(char c: t.toCharArray()){
+        map2.put(c, map2.getOrDefault(c,0)+1);
+    }
+
+    return map1.equals(map2);    
+    
     }
 }
